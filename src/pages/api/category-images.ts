4ec758828cf8de1b02a4951c699d1a3ed9aeed77
Anas <anas.alkhateeb.08@gmail.com>
2025-12-01 23:19:45 +0300

    Update CMS config - remove Gifts, add Miscellaneous

---
+++ src/pages/api/category-images.ts
@@ -0,0 +1,26 @@
+﻿import type { APIRoute } from 'astro';
+import { getCollection } from 'astro:content';
+
+export const GET: APIRoute = async ({ url }) => {
+  const category = url.searchParams.get('category');
+  
+  if (!category) {
+    return new Response(JSON.stringify([]), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' }
+    });
+  }
+
+  const posts = await getCollection('blog', ({ data }) => {
+    return data.category === category;
+  });
+
+  const images = posts
+    .filter(post => post.data.image)
+    .map(post => post.data.image);
+
+  return new Response(JSON.stringify(images), {
+    status: 200,
+    headers: { 'Content-Type': 'application/json' }
+  });
+};
