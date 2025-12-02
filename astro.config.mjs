cc56c6d38e91da15b422db77983e19d99e8ce529
Anas <anas.alkhateeb.08@gmail.com>
2025-12-02 21:19:31 +0300

    Update to Tailwind v4 syntax

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://infoprods.com',
   integrations: [sitemap()],
+
   vite: {
     plugins: [tailwindcss()],
   },
